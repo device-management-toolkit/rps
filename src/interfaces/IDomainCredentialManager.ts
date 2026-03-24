aa255f86ede7c1ef98594788fc312558a7d40f1f
madhavilosetty-intel <madhavi.losetty@intel.com>
2026-03-24 13:39:37 -0700

    fix: ACM activation fails when device FQDN has more segments than stored domain suffix

--- src/interfaces/IDomainCredentialManager.ts
+++ src/interfaces/IDomainCredentialManager.ts
@@ -7,5 +7,5 @@ import { type AMTDomain } from '../models/index.js'
 
 export interface IDomainCredentialManager {
   getProvisioningCert: (domainSuffix: string, tenantId: string) => Promise<AMTDomain | null>
-  doesDomainExist: (domainSuffix: string, tenantId: string) => Promise<boolean>
+  doesDomainExist: (domainSuffix: string, tenantId: string) => Promise<AMTDomain | null>
 }
